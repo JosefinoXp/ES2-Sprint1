db3909a270826fb54f5dc85f199337e23d7733f7
Jose <joselucasxp345@gmail.com>
2025-10-07 21:26:44 -0300

    Teste de variavel e input

--- trabalho1/src/app/components/login/login.ts
+++ trabalho1/src/app/components/login/login.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Card } from "primeng/card";
 import { CardModule } from 'primeng/card';
 
 @Component({
   selector: 'app-login',
-  imports: [Card],
+  imports: [Card, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+  nomeUsuario: string = "";
+
+  senhaUsuario: string = "";
 
 }
